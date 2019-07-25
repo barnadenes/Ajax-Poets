7662dc5b929d2deaa7a1cb432ba1b3da414878d2
barnadenes <nyukk1007@gmail.com>
2019-07-26 01:47:56 +0200

    removed unnecessary classes, User Dao Done

--- src/main/java/com/codecool/web/dao/database/DatabaseUserDao.java
+++ src/main/java/com/codecool/web/dao/database/DatabaseUserDao.java
@@ -4,8 +4,6 @@ import com.codecool.web.dao.UserDao;
 import com.codecool.web.model.User;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public final class DatabaseUserDao extends AbstractDao implements UserDao {
 
@@ -13,24 +11,12 @@ public final class DatabaseUserDao extends AbstractDao implements UserDao {
         super(connection);
     }
 
-    public List<User> findAll() throws SQLException {
-        String sql = "SELECT id, email, password FROM users";
-        try (Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(sql)) {
-            List<User> users = new ArrayList<>();
-            while (resultSet.next()) {
-                users.add(fetchUser(resultSet));
-            }
-            return users;
-        }
-    }
-
     @Override
     public User findByEmail(String email) throws SQLException {
         if (email == null || "".equals(email)) {
             throw new IllegalArgumentException("Email cannot be null or empty");
         }
-        String sql = "SELECT id, email, password FROM users WHERE email = ?";
+        String sql = "SELECT user_id, email, password FROM users WHERE email = ?";
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, email);
             try (ResultSet resultSet = statement.executeQuery()) {
@@ -43,9 +29,10 @@ public final class DatabaseUserDao extends AbstractDao implements UserDao {
     }
 
     private User fetchUser(ResultSet resultSet) throws SQLException {
-        int id = resultSet.getInt("id");
+        int id = resultSet.getInt("user_id");
+        String name = resultSet.getString("name");
         String email = resultSet.getString("email");
         String password = resultSet.getString("password");
-        return new User(id, email, password);
+        return new User(id, name, email, password);
     }
 }
